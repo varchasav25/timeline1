3b7b851242fcba26f8b232280a96ea1bf55a2b21
varchasav25 <110874994+varchasav25@users.noreply.github.com>
2023-02-19 11:35:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        manrope:["manrope","sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
